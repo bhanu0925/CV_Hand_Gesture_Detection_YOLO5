7a11cff7ae2d71ca47101bc7e10397da99b82552
bhanu0925 <39095993+bhanu0925@users.noreply.github.com>
2022-04-17 23:18:49 +0530

    zipfile

--- capture_image.py
+++ capture_image.py
@@ -5,23 +5,28 @@ import uuid
 
 IMAGE_PATH='CollectedImages'
 
-labels=['Peace','HighFive','You','GoodJob','Dislike','PowerTo','OK','GoodLuck','CallMe','Victory','Rock']
+labels=['Peace','GoodJob','Dislike','OK','GoodLuck','CallMe','Victory']
 
-number_of_images=20
+number_of_images=30
 
 for label in labels:
     img_path = os.path.join(IMAGE_PATH, label)
     os.makedirs(img_path)
     cap=cv2.VideoCapture(0)
     print('Collecting images for {}'.format(label))
-    time.sleep(5)
-    for imgnum in range(number_of_images):
+    #time.sleep(2)
+    i=0
+    while(True):
+        #for imgnum in range(number_of_images):
         ret,frame=cap.read()
         imagename=os.path.join(IMAGE_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
         cv2.imwrite(imagename,frame)
         cv2.imshow('frame',frame)
         time.sleep(2)
+        i=i+1
+        print(i)
         
         if cv2.waitKey(1) & 0xFF==ord('q'):
+            i=0
             break
     cap.release()
